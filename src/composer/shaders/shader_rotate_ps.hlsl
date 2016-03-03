35242641562addbbbc2cddaad21c76675c38b71b
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2016-03-03 09:52:56 +0200

    added rotation

--- src/composer/shaders/shader_rotate_ps.hlsl
+++ src/composer/shaders/shader_rotate_ps.hlsl
@@ -5,32 +5,27 @@ struct vs_samples_output
 };
 
 Texture2D		photo_model: register(t0);
-
 SamplerState	photo_sampler : register(s0);
 
-/*
-//jfif tags for rotation, that come out from cameras
-enum orientation : uint32_t
-{
-    horizontal = 1,                      // 1 = Horizontal(normal)
-    mirror_horizontal = 2,               // 2 = Mirror horizontal
-    rotate_180 = 3,                      // 3 = Rotate 180
-    mirror_vertical = 4,                 // 4 = Mirror vertical
-    mirror_horizontal_rotate_270_cw = 5, // 5 = Mirror horizontal and rotate 270 CW
-    rotate_90_cw = 6,                    // 6 = Rotate 90 CW
-    miror_horizontal_rotate_90_cw = 7,   // 7 = Mirror horizontal and rotate 90 CW
-    rotate_270_cw = 8                    // 8 = Rotate 270 CW
-};
-*/
 
-float2x2 orientation_horizontal()
+#define orientation_horizontal 1                         // 1 = Horizontal(normal)
+#define orientation_mirror_horizontal 2                  // 2 = Mirror horizontal
+#define orientation_rotate_180  3                        // 3 = Rotate 180
+#define orientation_mirror_vertical 4                    // 4 = Mirror vertical
+#define orientation_mirror_horizontal_rotate_270_cw 5    // 5 = Mirror horizontal and rotate 270 CW
+#define orientation_rotate_90_cw 6                       // 6 = Rotate 90 CW
+#define orientation_miror_horizontal_rotate_90_cw  7     // 7 = Mirror horizontal and rotate 90 CW
+#define orientation_rotate_270_cw 8                      // 8 = Rotate 270 CW
+
+
+float2x2 horizontal_matrix()
 {
     return float2x2(
         float2 (1.0f, 0.0f),
         float2 (0.0f, 1.0f));
 }
 
-float2x2 mirror_horizontal()
+float2x2 mirror_horizontal_matrix()
 {
     return float2x2(
         float2 (-1.0f, 0.0f),
@@ -42,48 +37,70 @@ cos  sin
 -sin cos
 */
 
-float2x2 rotate_180()
+float2x2 rotate_180_matrix()
 {
     return float2x2(
         float2 (-1.0f, 0.0f),
         float2 (0.0f, -1.0f));
 }
 
-float2x2 mirror_vertical()
+float2x2 mirror_vertical_matrix()
 {
     return float2x2(
         float2 (1.0f, 0.0f),
         float2 (0.0f, -1.0f));
 }
 
-float2x2 rotate_270_cw()
-{
-    return float2x2(
-        float2 (0.0f, -1.0f),
-        float2 (1.0f, 0.0f));
-}
-
-float2x2 mirror_horizontal_rotate_270_cw()
-{
-    return mirror_horizontal() * rotate_270_cw();
-}
-
-float2x2 rotate_90_cw()
+float2x2 rotate_270_cw_matrix()
 {
     return float2x2(
         float2 (0.0f, 1.0f),
         float2 (-1.0f, 0.0f));
 }
 
-float2x2 miror_horizontal_rotate_90_cw()
+float2x2 mirror_horizontal_rotate_270_cw_matrix()
 {
-    return mirror_horizontal() * rotate_90_cw();
+    return mirror_horizontal_matrix() * rotate_270_cw_matrix();
+}
+
+float2x2 rotate_90_cw_matrix()
+{
+    return float2x2(
+        float2 (0.0f, -1.0f),
+        float2 (1.0f, 0.0f));
+}
+
+float2x2 miror_horizontal_rotate_90_cw_matrix()
+{
+    return mirror_horizontal_matrix() * rotate_90_cw_matrix();
+}
+
+float2x2 exif_rotation_matrix(uint orientation)
+{
+    switch (orientation)
+    {
+        case orientation_horizontal: return horizontal_matrix();
+        case orientation_mirror_horizontal: return mirror_horizontal_matrix();
+        case orientation_rotate_180: return rotate_180_matrix();
+        case orientation_mirror_vertical: return mirror_vertical_matrix();
+        case orientation_mirror_horizontal_rotate_270_cw: return mirror_horizontal_rotate_270_cw_matrix();
+        case orientation_rotate_90_cw: return rotate_90_cw_matrix();
+        case orientation_miror_horizontal_rotate_90_cw: return miror_horizontal_rotate_90_cw_matrix();
+        case orientation_rotate_270_cw: return rotate_270_cw_matrix();
+    }
 }
 
 float4 main(in vs_samples_output i) : sv_target0
 {
-    float2 uv2 = float2 (1.0f - i.m_uv0.y, i.m_uv0.x);
-    float3 f0 = photo_model.Sample(photo_sampler, uv2).xyz;
+    
+    float2 h   = float2(0.5f, 0.5f);
+    float2 uv  = i.m_uv0 - h;
+    float2 uv2 = mul(uv, exif_rotation_matrix( orientation_rotate_270_cw ) );
+    float2 uv3 = uv2 + h;
+    
+    //float2 uv3 = float2 (1.0f - i.m_uv0.y, i.m_uv0.x);
+
+    float3 f0 = photo_model.Sample(photo_sampler, uv3).xyz;
     return float4( f0, 1.0f);
 }
 
